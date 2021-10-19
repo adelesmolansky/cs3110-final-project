b081c3cebe02dbb7b636ef6de79bc844b8724ce5
David Thuman <dmt93@cornell.edu>
2021-10-19 09:40:03 -0400

    Non-working code, attempting to implement TCP connection to be able to read and write from the Terminal

--- src/client_view.ml
+++ src/client_view.ml
@@ -1,14 +1,23 @@
 open Async
-open Server
 open ANSITerminal
 
 (* [st] is the initial state of the client. *)
-let st = ref (init_state ())
+let st = "ref (init_state ())"
 
 let caml = ""
 
 let help_message = ""
 
-let main () = print_string [ Background Blue ] "Please help me!"
+let stuff = Tcp.connect_sock
+let tcp = let addr = Async.Tcp.with_connection (* Looking for a Socket.Address.t *) (* A function that will take in the created Writer.t and Reader.t that will finally read Terminal inputs *)
+
+let reg = [ Foreground White ]
+
+let main () =
+  print_string reg "Welcome to Caml Chat!\n";
+  print_string reg "Please enter your username,\n";
+  print_string [ Foreground Blue ] ">> ";
+  Command.async Command.Spec
+
 
 let () = main ()
