1e085c1b2531fe519176a7303b3c8e75637ece7a
David Thuman <dmt93@cornell.edu>
2021-10-20 11:13:35 -0400

    Possible functioning client_view.ml, need to hard code in working host and port pair

--- src/client_view.ml
+++ src/client_view.ml
@@ -73,20 +73,16 @@ let tcp host port =
   let addr =
     Async.Tcp.Where_to_connect.of_host_and_port { host; port }
   in
-  ignore (Async.Tcp.with_connection addr chat);
-  Deferred.never ()
-
-(* Looking for a Socket.Address.t *)
-(* A function that will take in the created Writer.t and Reader.t that
-   will finally read Terminal inputs *)
+  Async.Tcp.with_connection addr chat
 
 let reg = [ Foreground White ]
 
-(** Command.async Command.Param *)
 let main () =
   print_string reg "Welcome to Caml Chat!\n";
   print_string reg "Please enter your username,\n";
   print_string [ Foreground Blue ] ">> ";
-  Command.async tcp "" 9999 |> Command.run
+  Command.async ~summary:""
+    (Command.Param.return (fun () -> tcp "127.0.0.1" 8080))
+  |> Command.run
 
 let () = main ()
